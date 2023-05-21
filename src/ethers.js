b7cefda774fc884f72442ba3547ad9665e690aa5
lajosdeme <lajosd@protonmail.ch>
2023-05-21 12:13:25 +0200

    update smart contract methods in ethers

--- src/ethers.js
+++ src/ethers.js
@@ -8,7 +8,7 @@ class Ethers {
         this.provider = provider
         this.signer = this.provider.getSigner();
 
-        this.employer = new ethers.Contract("0x773DEfAaEFb3F9cf5F570f6fB7726F92c7d8eec6", employerAbi, this.signer);
+        this.employer = new ethers.Contract("0x81C95c0Ea2E6D01adF057E791B40eE547e929Acc", employerAbi, this.signer);
 
         window.ethereum.on("accountsChanged", async () => {
             await this.connect();
@@ -24,12 +24,12 @@ class Ethers {
         console.log("Connected address is: ", this.address)
     }
 
-    addVerifiedEmployee = async(response) => {
+    addVerifiedEmployee = async (response) => {
         try {
             const tx = await this.employer.addVerifiedEmployee(response);
             await tx.wait();
             return true;
-        } catch(e) {
+        } catch (e) {
             if (e.error === (undefined)) {
                 console.log("error: ", e.message)
                 alert(e.message)
@@ -41,7 +41,75 @@ class Ethers {
         }
     }
 
-    isVerifiedEmployee = async(address) => {
+    provisionEmployee = async (token, amount, employee, expiry, allowlist) => {
+        try {
+            const tx = await this.employer.provisionEmployee(token, amount, employee, allowlist);
+            await tx.wait();
+            return true;
+        } catch (e) {
+            if (e.error === (undefined)) {
+                console.log("error: ", e.message)
+                alert(e.message)
+                return false
+            }
+            console.log("an error occurred: ", e.error.message)
+            alert(e.error.message)
+            return false
+        }
+    }
+
+    updateProvision = async(provisionId, amount, expiry, allowlist, denylist) => {
+        try {
+            const tx = await this.employer.updateProvision(provisionId, amount, expiry, allowlist, denylist);
+            await tx.wait();
+            return true;
+        }  catch (e) {
+            if (e.error === (undefined)) {
+                console.log("error: ", e.message)
+                alert(e.message)
+                return false
+            }
+            console.log("an error occurred: ", e.error.message)
+            alert(e.error.message)
+            return false
+        }
+    }
+
+    spendProvision = async(token, amount, recipient) => {
+        try {
+            const tx = await this.employer.spendProvision(token, amount, recipient);
+            await tx.wait();
+            return true;
+        } catch (e) {
+            if (e.error === (undefined)) {
+                console.log("error: ", e.message)
+                alert(e.message)
+                return false
+            }
+            console.log("an error occurred: ", e.error.message)
+            alert(e.error.message)
+            return false
+        }
+    }
+
+    provideRailgunAddr = async(addr) => {
+        try {
+            const tx = await this.employer.provideRailgunAddr(addr);
+            await tx.wait();
+            return true;
+        } catch (e) {
+            if (e.error === (undefined)) {
+                console.log("error: ", e.message)
+                alert(e.message)
+                return false
+            }
+            console.log("an error occurred: ", e.error.message)
+            alert(e.error.message)
+            return false
+        }
+    }
+
+    isVerifiedEmployee = async (address) => {
         const isVerified = await this.employer.isVerifiedEmployee(address);
         return isVerified;
     }
